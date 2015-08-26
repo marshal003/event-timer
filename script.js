d7acfb1b5a6081ecef92acd6c425fdab5787cf42
Vinit Kumar Rai <vinitkumar.rai@hashedin.com>
2015-08-26 21:25:55 +0530

    Added another version of event timer

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
   
   It uses Jquery only for DOM manipulation, but again that can be simple done
   using native APIs.
+  http://plnkr.co/edit/QTLCXiqCdLgPrgbrN44d?p=preview
   
     
 */
@@ -17,26 +18,25 @@
     return "08-31-2015 23:59:59";
   };
   
-  var currentDate = new Date().getTime(),
-    eventDateTimeStamp = new Date(getEventDate()).getTime(),
-    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"];
+  var calculateDiffs = function() {
+    var currentDate = new Date().getTime();
+    var eventDateTimeStamp = new Date(getEventDate()).getTime();
+    var totalMilliSecondsRemaining = eventDateTimeStamp - currentDate;
+    return totalMilliSecondsRemaining / 1000;
+  };
+
+  var totalSecondsRemaining = calculateDiffs();
+  var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"];
   
   
   var counter = function(){
-    currentDate = new Date();
-    updateDOM();
+    totalSecondsRemaining = totalSecondsRemaining - 1;
+    updateDomWithRemainingTime();
     checkEventDueDate();
   };
   
-  var updateDOM = function(){
-    $("#days").html(currentDate.getDate());
-    $("#hours").html(currentDate.getHours());
-    $("#minutes").html(currentDate.getMinutes());
-    $("#seconds").html(currentDate.getSeconds());
-    $("#current-date").html(getDateString());
-  };
-  
   var getDateString = function(){
+    var currentDate = new Date();
     var month = months[currentDate.getMonth()],
       date = currentDate.getDate(),
       year = currentDate.getFullYear();
@@ -45,10 +45,24 @@
   };
   
   var checkEventDueDate = function(){
-    if (currentDate.getTime() >= eventDateTimeStamp) {
+    if (totalSecondsRemaining <= 0) {
       clearInterval(timer);
     }  
   };
 
+  
+  var updateDomWithRemainingTime = function(){
+    var seconds = Math.floor(totalSecondsRemaining % 60),
+      minutes = Math.floor(totalSecondsRemaining / 60) % 60,
+      hours = Math.floor(totalSecondsRemaining / (60 * 60)) % 60,
+      days = Math.floor(totalSecondsRemaining / (24 * 60 * 60)) % 24;
+    
+    $("#days").html(days);
+    $("#hours").html(hours);
+    $("#seconds").html(seconds);
+    $("#minutes").html(minutes);
+    $("#current-date").html(getDateString());
+    
+  };
+  
   var timer = setInterval(counter, 1000);
-
